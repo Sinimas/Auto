fe4eb3aa47f160f0944f5f7a617eb44aec93a5ce
Sinimas <102421245+Sinimas@users.noreply.github.com>
2022-03-26 20:31:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{filename_name} \n @primetimemovies,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Share Group', url="https://t.me/+u1--j-zaMGc5NTll"
                                 )
                         ]
                     ]
